a17cfe2b904a8ffbca869a9ff5058839c246a725
ramonrsv <ramonesibello@gmail.com>
2017-11-19 01:18:04 -0500

    Reworking SymbolTable

--- Lab_3/Starter3/symbol.cpp
+++ Lab_3/Starter3/symbol.cpp
@@ -27,49 +27,54 @@ int Type::getBaseType(int _type, int vec_size) {
     }
 }
 
-Symbol::Symbol() :m_type(ANY_T) {}
+Symbol::Symbol()
+        :m_isConst(false), m_type(ANY_T) {}
 
-SymbolTable::SymbolTable() :m_currentScopeNum(0) {}
+Symbol::Symbol(bool _isConst, std::string &_id, Type _type)
+        :m_isConst(_isConst), m_ID(_id), m_type(_type) {}
 
-int SymbolTable::enterScope() {
-    m_symbolTable.push_back(SymbolScope());
-    return ++m_currentScopeNum;
+
+//SymbolTable ----------------------------------------------------
+SymbolTable::SymbolTable()
+        :m_currentScopeID(nullptr) {}
+
+void SymbolTable::enterScope(ScopeID scope_id) {
+    m_currentScopeID = scope_id;
+    if (m_cache.find(m_currentScopeID) == m_cache.end())
+        m_cache[m_currentScopeID] = SymbolScope();
+    m_currentScopeStack.push_back(IDScopePair(m_currentScopeID, m_cache[m_currentScopeID]));
 }
 
-int SymbolTable::exitScope() {
-    m_symbolTable.pop_back();
-    return --m_currentScopeNum;
+void SymbolTable::exitScope() {
+    m_cache[m_currentScopeID] = m_currentScopeStack.back().second;
+    m_currentScopeStack.pop_back();
+    m_currentScopeID = (m_currentScopeStack.empty()? nullptr : m_currentScopeStack.back().first);
 }
 
 SymbolScope& SymbolTable::currentScope() {
-    return m_symbolTable.back(); //may throw if no scope has been entered yet
+    return m_currentScopeStack.back().second;
 }
 
-int SymbolTable::currentScopeNum() {
-    return m_currentScopeNum;
-}
-
-bool SymbolTable::pushElement(Symbol element) {
-    if (currentScope().find(element.m_ID) != currentScope().end())
-        return false; //Element with this name already present
-    else {
-        currentScope()[element.m_ID] = element;
-        return true;
-    }
-}
-
-bool SymbolTable::findElement(std::string& _ID) {
-    for (auto scope = m_symbolTable.rbegin(); scope != m_symbolTable.rend(); scope++) {
-        if (scope->find(_ID) != scope->end())
+bool SymbolTable::findElementInStack(std::string& _ID) {
+    for (auto id_scope = m_currentScopeStack.rbegin(); id_scope != m_currentScopeStack.rend(); id_scope++) {
+        if (id_scope->second.find(_ID) != id_scope->second.end())
             return true;
     }
     return false;
 }
 
-Symbol SymbolTable::getElement(std::string &_ID) { //may throw if element not found, call findElement first
-    for (auto scope = m_symbolTable.rbegin(); scope != m_symbolTable.rend(); scope++) {
-        if (scope->find(_ID) != scope->end())
-            return scope->find(_ID)->second;
+bool SymbolTable::findElementInCurrentScope(std::string &_ID) {
+    return (currentScope().find(_ID) != currentScope().end());
+}
+
+void SymbolTable::pushElement(Symbol element) {
+    currentScope()[element.id()] = element;
+}
+
+Symbol SymbolTable::getElementInStack(std::string &_ID) {
+    for (auto id_scope = m_currentScopeStack.rbegin(); id_scope != m_currentScopeStack.rend(); id_scope++) {
+        if (id_scope->second.find(_ID) != id_scope->second.end())
+            return id_scope->second.at(_ID);
     }
     throw std::runtime_error("SymbolTable::getElement: Symbol " + _ID + " not found in symbol table");
 }
--- Lab_3/Starter3/symbol.hpp
+++ Lab_3/Starter3/symbol.hpp
@@ -24,33 +24,45 @@ private:
     static int getBaseType(int _type, int vec_size);
 };
 
-struct Symbol
+class Symbol
 {
 public:
     Symbol();
+    Symbol(bool _isConst, std::string& _id, Type _type);
+
+    bool isConst() const {return m_isConst;}
+    std::string id() const {return m_ID;}
+    Type type() const {return m_type;}
+
+private:
+    bool m_isConst;
     std::string m_ID;
     Type m_type;
 };
 
-//Note that here scope refers to any scope, whether {} or IF, IF-ELSE statements
+class Scope;
+typedef Scope* ScopeID;
 typedef std::map<std::string, Symbol> SymbolScope;
+typedef std::map<ScopeID, SymbolScope> SymbolTableCache;
+typedef std::pair<ScopeID, SymbolScope> IDScopePair;
 
 class SymbolTable
 {
 public:
     SymbolTable();
-    int enterScope(); //Returns new scope number
-    int exitScope(); //Returns new scope number
-    SymbolScope& currentScope();
-    int currentScopeNum();
+    void enterScope(ScopeID scope_id);
+    void exitScope();
+    SymbolScope& currentScope(); //May throw if no scope has been entered yet
 
-    bool pushElement(Symbol element);
-    bool findElement(std::string& _ID);
-    Symbol getElement(std::string& _ID);
+    bool findElementInStack(std::string& _ID);
+    bool findElementInCurrentScope(std::string& _ID);
+    void pushElement(Symbol element); //Overwrites an element if exists
+    Symbol getElementInStack(std::string& _ID); //May throw if element not found, call findElement first
 
 private:
-    int m_currentScopeNum;
-    std::vector<SymbolScope> m_symbolTable;
+    ScopeID m_currentScopeID;
+    std::vector<IDScopePair> m_currentScopeStack;
+    SymbolTableCache m_cache;
 };
 
 #endif
