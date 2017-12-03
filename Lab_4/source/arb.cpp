84eae8ab449782863eb97723abf4509c9f235ce3
ramonrsv <ramonesibello@gmail.com>
2017-12-02 19:43:57 -0500

    Implementing several helper classes in 'arb.hpp/cpp' [NOT TESTED]

--- Lab_4/source/arb.cpp
+++ Lab_4/source/arb.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "arb.hpp"
+
+int ARBVar::m_uniqueID = 0;
+
+const std::map<std::string, std::string> ARBVar::m_preDefinedID_to_ARBID {
+        {"gl_FragColor", "result.color"},
+        {"gl_FragDepth", "result.depth"},
+        {"gl_FragCoord", "fragment.position"},
+        {"gl_TexCoord", "fragment.texcoord"},
+        {"gl_Color", "fragment.color"},
+        {"gl_Secondary", "fragment.color.secondary"},
+        {"gl_FogFragCoord", "fragment.fogcoord"},
+        {"gl_Light_Half", "state.light[0].half"},
+        {"gl_Light_Ambient", "state.lightmodel.ambient"},
+        {"gl_Material_Shininess", "state.material.shininess"},
+        {"env1", "program.env[1]"},
+        {"env2", "program.env[2]"},
+        {"env3", "program.env[3]"}
+};
+
+ARBVar::ARBVar()
+        :m_ID("temp_var_" + std::to_string(m_uniqueID++)) {}
+
+ARBVar::ARBVar(std::string pre_defined_id)
+        :m_ID(m_preDefinedID_to_ARBID.at(pre_defined_id)) {}
+
+ARBInstructionSequence& ARBInstructionSequence::push(ARBInstruction &instruction) {
+    m_instructions.push_back(instruction);
+    return *this;
+}
+
+ARBInstructionSequence& ARBInstructionSequence::push(ARBInstructions &instructions) {
+    m_instructions.insert(m_instructions.end(), instructions.begin(), instructions.end());
+    return *this;
+}
+
+ARBInstructionSequence& ARBInstructionSequence::push(ARBInstructionSequence &instruction_sequence) {
+    push(instruction_sequence.m_instructions);
+    return *this;
+}
--- Lab_4/source/arb.hpp
+++ Lab_4/source/arb.hpp
@@ -5,16 +5,60 @@
 #ifndef ARB_INSTRUCTIONS_H
 #define ARB_INSTRUCTIONS_H
 
+#include <string>
+#include <map>
 #include "symbol.hpp"
+
+class ARBVar
+{
+public:
+    ARBVar();
+    ARBVar(std::string pre_defined_id);
+    std::string id() { return m_ID; }
+    bool empty() { return m_ID.empty(); }
+
+private:
+    std::string m_ID;
+    static int m_uniqueID;
+    static const std::map<std::string, std::string> m_preDefinedID_to_ARBID;
+};
+typedef std::vector<ARBVar> ARBVars;
+
+enum class ARBInstID {
+    ABS, ADD, CMP, COS, DP3, DP4, DPH, DST, EX2, FLR, FRC, KIL,
+    LG2, LIT, LRP, MAD, MAX, MIN, MOV, MUL, POW, RCP, RSQ, SCS,
+    SGE, SIN, SLT, SUB, SWZ, TEX, TXB, TXP, XPD,
+    UNKNOWN //debugging
+};
 
 class ARBInstruction
 {
+public:
+    ARBInstruction(ARBInstID _id, ARBVar _result, ARBVars _arguments)
+            :m_ID(_id), m_resultVar(_result), m_argumentVars(_arguments) {}
 
+    ARBInstID id() { return m_ID; }
+    ARBVar resultVar() { return m_resultVar; }
+    ARBVars argVars() { return m_argumentVars; }
+
+private:
+    ARBInstID m_ID;
+    ARBVar m_resultVar;
+    std::vector<ARBVar> m_argumentVars;
 };
+typedef std::vector<ARBInstruction> ARBInstructions;
+
 
 class ARBInstructionSequence
 {
+public:
+    const ARBInstructions& instructions() { return m_instructions; }
+    ARBInstructionSequence& push(ARBInstruction& instruction);
+    ARBInstructionSequence& push(ARBInstructions& instructions);
+    ARBInstructionSequence& push(ARBInstructionSequence& instruction_sequence);
 
+private:
+    std::vector<ARBInstruction> m_instructions;
 };
 
 #endif //ARB_INSTRUCTIONS_H
